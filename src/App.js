7cb16cd1591b37abb8ee114363436ef4e078e6e2
ht1204 <ptb1204@hotmail.com>
2023-06-12 22:25:35 -0500

    feature: ajustes funcionales para pasar unit testing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,34 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      repetir: '',
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event){
+    this.setState({
+      repetir: event.target.value,
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input
+          type="text"
+          placeholder="Empieza a escribir algo"
+          value={this.state.repetir}
+          onChange={this.handleInputChange}
+        />
+        <p className="repeater">
+          {this.state.repetir}
+        </p>
       </div>
     );
   }
